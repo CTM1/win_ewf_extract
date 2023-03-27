418bd981f5f78896318d705ae0f0c614fe657512
CTM1 <45026430+CTM1@users.noreply.github.com>
2023-03-27 21:40:26 +0200

    feat(module): Registry first attempt

--- modules/registry.py
+++ modules/registry.py
@@ -1,15 +1,18 @@
-import pytsk3
+import pytsk
 import csv
 
 def extract(ewf_file, keys=None, output_dir="./output"):
-    # Open the Encase file
     img = pytsk3.Img_Info(ewf_file)
-
-    # Open the Windows Registry hive
     reg = pytsk3.Registry_Info(img)
 
     if keys is None:
         keys = [key.path() for key in reg.recurse_subkeys()]
 
-    for key in keys:
-        print(key)
+    with open(csv_file, "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+    
+        writer.writerow(["Registry Key", "Value", "Type", "Created At", "Modified At", "Deleted At"])
+
+        for key in registry_keys:
+            value = key.value()        
+            writer.writerow([key.path(), value.data(), value.type(), key.info.crtime, key.info.mtime, key.info.dtime])
