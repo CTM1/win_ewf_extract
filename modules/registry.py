2a4777777c8739a6fcaf1a47f60808946ab4bc43
CTM1 <krkn@cruiser.com>
2023-03-27 21:11:20 +0200

    feat(module): Started work on the registry module

--- modules/registry.py
+++ modules/registry.py
@@ -1,6 +1,7 @@
 import pytsk3
+import csv
 
-def extract(ewf_file, keys=None):
+def extract(ewf_file, keys=None, output_dir="./output"):
     # Open the Encase file
     img = pytsk3.Img_Info(ewf_file)
 
@@ -9,6 +10,6 @@ def extract(ewf_file, keys=None):
 
     if keys is None:
         keys = [key.path() for key in reg.recurse_subkeys()]
-    
+
     for key in keys:
         print(key)
