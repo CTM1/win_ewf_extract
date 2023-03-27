e9196c80404a82a5402a89f8d47f5b59d0efbec4
CTM1 <krkn@cruiser.com>
2023-03-27 21:05:06 +0200

    feat(args): Added more argument parsing and help messages

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import importlib
 import argparse
 import yaml
+import os
 
 def main():
     # Arguments
     parser = argparse.ArgumentParser(description='Windows EWF Artifact Extractor')
-    parser.add_argument('cfg', help='YAML configuration file - Possible fields: extract_registry\nextract_browsers\nextract_event_logs\nextract_mft')
-    parser.add_argument('ewf_file', help='Path to Encase Windows file')
+    parser.add_argument("-c","--cfg", type=str, help='YAML configuration file - Possible fields: extract_registry\nextract_browsers\nextract_event_logs\nextract_mft')
+    parser.add_argument("-o", "--output", type=str, help="Output directory for extracted artifacts - ./output by default")
+    parser.add_argument("-f","--ewf_file", type=str, help='Path to Encase Windows file')
     args = parser.parse_args()
+
+    output_dir = args.output or "output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     # Load configuration from a YAML file
     with open(args.cfg) as f:
