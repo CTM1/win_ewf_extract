3525dca8574efd7151cc26149333edc6f29087d2
CTM1 <45026430+CTM1@users.noreply.github.com>
2023-04-03 01:24:55 +0200

    refactor(Extractors): A few comments

--- modules/artifact_extraction.py
+++ modules/artifact_extraction.py
@@ -1,3 +1,4 @@
 class ArtifactExtractor:
     def process_fs_object(self, fs_object):
         raise NotImplementedError("Subclasses must implement this method.")
+    # TODO: set starting_path, processable_file_names and processable_directories as mandatory attributes.
