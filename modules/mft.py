d80ef8b08fea49171200fb417b1e593d341548a8
Olivier Moreau <olivier.moreau@aktea.fr>
2023-04-09 12:14:50 +0200

    doc

--- modules/mft.py
+++ modules/mft.py
@@ -5,11 +5,8 @@ from analyzemft import mftsession
 
 
 class MftExtractor(ArtifactExtractor):
-    """This class extracts the $MFT file and information from it into a .csv file
-
-    Args:
-        ArtifactExtractor (class): Superclass of all Extractors
-    """
+    """MftExtractor is an implementation of ArtifactExtractor dedicated to
+    extracting the MFT."""
     def __init__(self, output_dir, config):
         self.n_mft = 0
         # Path to output directory
